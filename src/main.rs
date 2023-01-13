a126fa90d49a0e7ae33c1018e01b5c3cf2351fad
Sky Brewer <jaco.brewer@gmail.com>
2023-01-13 19:11:43 +0100

    two sphere main

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ fn main() -> io::Result<()> {
     let dim = Dimensions::new(1280, 960);
     let mut image = Image::new(dim);
     let scene = Scene::new(
-        vec![Box::new(Sphere::new(
-            Vector3D::new(0.0, 0.0, 0.0),
-            10.0,
-            Coloring::Fill(Color::RED),
-        ))],
+        vec![
+            Box::new(Sphere::new(
+                Vector3D::new(0.0, 0.0, 0.0),
+                10.0,
+                Coloring::Fill(Color::RED),
+            )),
+            Box::new(Sphere::new(
+                Vector3D::new(00.0, 0.0, 10.0),
+                20.0,
+                Coloring::Fill(Color::YELLOW),
+            )),
+        ],
         Camera::new(
             Vector3D::new(0.0, 0.0, -20.0),
             Vector3D::new(0.0, 1.0, 0.0),
