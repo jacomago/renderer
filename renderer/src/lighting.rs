46e00ff3060c89020fd6a573257eed630b0284d2
Sky Brewer <jaco.brewer@gmail.com>
2023-01-14 14:28:48 +0100

    merge float color

--- renderer/src/lighting.rs
+++ renderer/src/lighting.rs
@@ -1,14 +1,14 @@
+use colors::prelude::Color;
 use geometry::prelude::Vector3D;
-use image_lib::prelude::Color;
 
 pub struct Light {
     position: Vector3D<f32>,
-    color: Color,
-    intensity: Color,
+    color: Color<f32>,
+    intensity: Color<f32>,
 }
 
 impl Light {
-    pub fn new(position: Vector3D<f32>, color: Color, intensity: Color) -> Self {
+    pub fn new(position: Vector3D<f32>, color: Color<f32>, intensity: Color<f32>) -> Self {
         Self {
             position,
             color,
