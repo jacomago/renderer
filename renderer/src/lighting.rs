3c93e956855d82be77e8c6581d1a2ac3de5568af
Sky Brewer <jaco.brewer@gmail.com>
2023-01-15 06:03:20 +0100

    more refactor

--- renderer/src/lighting.rs
+++ renderer/src/lighting.rs
@@ -1,7 +1,5 @@
 use colors::prelude::Color;
-use geometry::prelude::{instersections, Normal, Ray, Vector3D};
-
-use crate::object::SceneObject;
+use geometry::prelude::{Ray, Vector3D};
 
 pub struct Light {
     position: Vector3D<f32>,
@@ -20,77 +18,10 @@ impl Light {
     pub fn color(&self) -> &Color<f32> {
         &self.color
     }
+    pub fn position(&self) -> &Vector3D<f32> {
+        &self.position
+    }
     pub fn ray(&self, position: &Vector3D<f32>) -> Ray {
         Ray::new(self.position, self.position - *position)
-    }
-
-    pub fn angle(&self, shape: &dyn Normal<f32>, position: &Vector3D<f32>) -> f32 {
-        let r = self.position - *position;
-        let normalized_r = r.normalize();
-        normalized_r.dot(&shape.normal(position))
-    }
-
-    pub fn light_ray_intersect_other_object(
-        &self,
-        objects: &[SceneObject],
-        position: &Vector3D<f32>,
-        scene_object: &SceneObject,
-    ) -> bool {
-        let ray = self.ray(position);
-        if instersections(
-            &objects
-                .iter()
-                .filter(|o| *o != scene_object)
-                .copied()
-                .collect::<Vec<SceneObject>>(),
-            &ray,
-        )
-        .is_empty()
-        {
-            return false;
-        }
-        true
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use colors::prelude::Coloring;
-    use geometry::prelude::Sphere;
-
-    use super::*;
-
-    #[test]
-    fn test_light_ray_intersect_none() {
-        let object = SceneObject::new(Sphere::new(Vector3D::default(), 1.0), Coloring::default());
-        let light = Light::new(
-            Vector3D::new(1.0, 1.0, 1.0),
-            Color::default(),
-            Color::default(),
-        );
-        let position = Vector3D::default();
-
-        assert!(!light.light_ray_intersect_other_object(&[object], &position, &object));
-    }
-
-    #[test]
-    fn test_light_ray_intersect_one() {
-        let object = SceneObject::new(Sphere::new(Vector3D::default(), 1.0), Coloring::default());
-        let intersection_object = SceneObject::new(
-            Sphere::new(Vector3D::new(2.0, 0.0, 0.0), 1.0),
-            Coloring::default(),
-        );
-        let light = Light::new(
-            Vector3D::new(3.0, 0.0, 0.0),
-            Color::default(),
-            Color::default(),
-        );
-        let position = Vector3D::default();
-
-        assert!(light.light_ray_intersect_other_object(
-            &[object, intersection_object],
-            &position,
-            &object
-        ));
     }
 }
--- renderer/src/surface.rs
+++ renderer/src/surface.rs
@@ -1,25 +1,52 @@
 use colors::prelude::Color;
-use geometry::prelude::Vector3D;
+use geometry::prelude::{instersections, Normal, Ray, Vector3D};
 
 use crate::{
     colored::Colored,
     prelude::{Light, SceneObject},
 };
 
+fn angle(ray: &Ray, shape: &dyn Normal<f32>, position: &Vector3D<f32>) -> f32 {
+    let r = ray.direction;
+    let normalized_r = r.normalize();
+    normalized_r.dot(&shape.normal(position))
+}
+
+fn ray_intersect_other_object(
+    ray: &Ray,
+    objects: &[SceneObject],
+    scene_object: &SceneObject,
+) -> bool {
+    if instersections(
+        &objects
+            .iter()
+            .filter(|o| *o != scene_object)
+            .copied()
+            .collect::<Vec<SceneObject>>(),
+        ray,
+    )
+    .is_empty()
+    {
+        return false;
+    }
+    true
+}
+
 pub fn light_bounce_color(
     objects: &[SceneObject],
-    l: &Light,
+    light: &Light,
     position: &Vector3D<f32>,
     scene_object: &SceneObject,
 ) -> Option<Color<f32>> {
-    if l.light_ray_intersect_other_object(objects, position, scene_object) {
+    let ray = light.ray(position);
+    if ray_intersect_other_object(&ray, objects, scene_object) {
         return None;
     }
     let light_color = surface_color(
-        l,
+        light,
         scene_object,
         position,
-        l.angle(scene_object.shape(), position),
+        angle(&ray, scene_object.shape(), position),
     );
     Some(light_color)
 }
@@ -32,4 +59,36 @@ fn surface_color(
 ) -> Color<f32> {
     let shape_color = shape.color(position).unwrap_or_default();
     *light.color() * shape_color * surface_coeff
+}
+
+#[cfg(test)]
+mod tests {
+    use colors::prelude::Coloring;
+    use geometry::prelude::Sphere;
+
+    use super::*;
+
+    #[test]
+    fn test_ray_intersect_none() {
+        let object = SceneObject::new(Sphere::new(Vector3D::default(), 1.0), Coloring::default());
+        let ray = Ray::new(Vector3D::new(1.0, 0.0, 0.0), Vector3D::new(-1.0, 0.0, 0.0));
+
+        assert!(!ray_intersect_other_object(&ray, &[object], &object));
+    }
+
+    #[test]
+    fn test_ray_intersect_one() {
+        let object = SceneObject::new(Sphere::new(Vector3D::default(), 1.0), Coloring::default());
+        let intersection_object = SceneObject::new(
+            Sphere::new(Vector3D::new(2.0, 0.0, 0.0), 1.0),
+            Coloring::default(),
+        );
+        let ray = Ray::new(Vector3D::new(3.0, 0.0, 0.0), Vector3D::new(-1.0, 0.0, 0.0));
+
+        assert!(ray_intersect_other_object(
+            &ray,
+            &[object, intersection_object],
+            &object
+        ));
+    }
 }
